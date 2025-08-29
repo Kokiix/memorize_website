a1da829b41632ee5d9a8f7753071a7d8bd543231
Kenneth-Desktop <kokiidoki@gmail.com>
2025-08-29 18:32:40 -0400

    fix answer wipe before submission, level content within hero

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ memStringNode.textContent = '' + Math.floor(dailyRNG() * 10000);
 updateMemorizeString();
 
 function submitAnswer() {
-    inputGuess.value = ''
+    document.getElementById("hero-body").classList.add("level");
     if (inputGuess.value === memStringNode.textContent) {
         updateMemorizeString();
     } else {
@@ -15,14 +15,19 @@ function submitAnswer() {
         document.getElementById("lose_screen").style.display = "grid";
         document.getElementById("lose_screen").textContent += memStringNode.textContent.length
     }
+
+    
+    inputGuess.value = ''
 }
 
 async function updateMemorizeString() {
     memStringNode.textContent += Math.floor(dailyRNG() * 10);
     memStringNode.style.display = "grid";
     document.getElementById("game_form").style.display = "none";
+
     await new Promise(r => setTimeout(r, 3000));
     memStringNode.style.display = "none";
+    document.getElementById("hero-body").classList.remove("level");
     document.getElementById("game_form").style.display = "grid";
     inputGuess.focus();
 }
