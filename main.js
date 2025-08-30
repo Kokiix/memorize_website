82e25e2b487554c0fbe8c29f09bab108eb424834
Kenneth-Desktop <kokiidoki@gmail.com>
2025-08-29 21:53:10 -0400

    allow skipping ahead in daily number

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
 memStringNode = document.getElementById("memorize_string")
 inputGuess = document.getElementById("guess")
+gameForm = document.getElementById("game_form");
+
+gameForm.reset();
+inputGuess.value = 'x'.repeat(4);
 
 dailyRNG = new Math.seedrandom(new Date().toDateString());
-memStringNode.textContent = '' + Math.floor(dailyRNG() * 10000);
+
+score = 5
+memString = '' + Math.abs(dailyRNG.int32());
+memStringNode.textContent = memString.slice(0, score);
 updateMemorizeString();
+gameForm.reset();
 
 function submitAnswer() {
-    document.getElementById("hero-body").classList.add("level");
-    if (inputGuess.value === memStringNode.textContent) {
-        updateMemorizeString();
-    } else {
-        document.getElementById("game_form").style.display = "none";
-        memStringNode.style.display = "grid";
-        document.getElementById("lose_screen").style.display = "grid";
-        document.getElementById("lose_screen").textContent += memStringNode.textContent.length
+    if (inputGuess.value.length >= memStringNode.textContent.length) {
+        memString += Math.abs(dailyRNG.int32());
     }
 
+    document.getElementById("hero-body").classList.add("level");
+    console.log("Internal string to memorize " + memString);
+    console.log("Input string " + inputGuess.value);
     
+    if (memString.startsWith(inputGuess.value)) {
+        updateMemorizeString();
+    } else {
+        gameForm.style.display = "none";
+        memStringNode.style.display = "grid";
+        document.getElementById("lose_screen").style.display = "grid";
+        document.getElementById("lose_screen").textContent += " " + score;
+    }
+
     inputGuess.value = ''
 }
 
 async function updateMemorizeString() {
-    memStringNode.textContent += Math.floor(dailyRNG() * 10);
+    score = inputGuess.value.length + 1;
+    memStringNode.textContent = memString.slice(0, score);
     memStringNode.style.display = "grid";
-    document.getElementById("game_form").style.display = "none";
+    gameForm.style.display = "none";
 
     await new Promise(r => setTimeout(r, 3000));
     memStringNode.style.display = "none";
     document.getElementById("hero-body").classList.remove("level");
-    document.getElementById("game_form").style.display = "grid";
+    gameForm.style.display = "grid";
     inputGuess.focus();
 }
